1caec4947c341e3b67bcb8073124326d1c8a275d
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-26 03:08:12 +0800

    added login and register system but logout still not working

--- app/src/main/java/com/example/antiwaste3_0/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/antiwaste3_0/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.antiwaste3_0.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.antiwaste3_0.LoginActivity
 import com.example.antiwaste3_0.R
 import com.example.antiwaste3_0.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -35,6 +39,12 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        /*
+        btn_logout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }*/
+
         return root
     }
 
